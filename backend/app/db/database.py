e7b2f8c71d98ec7eb33e9aaec9caee1729d45e88
Aadhil21 <aadhil.lk@gmail.com>
2025-04-20 21:18:52 +0530

    Update req file

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -8,7 +8,7 @@ from core.config import Config
 
 DATABASE_URL = Config.DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
